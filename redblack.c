3690a98958471366a07970504ceed7f859ba9339
Unknown <leo_minari@hotmail.com>
2018-06-23 20:44:31 -0300

    trabalho v1.2
    adicionada redblack.c, primeiro teste.

--- redblack.c
+++ redblack.c
@@ -1,5 +1,178 @@
-#include "avl.h"
+#include "redblack.h"
 
-rb* novoNo(rb* arv, int x){
-  
+
+Tree* newNode(Tree *parent, unsigned long x){
+
+    Tree* tree = (Tree*)malloc(sizeof(Tree));
+    if (tree == NULL) {
+        puts("Erro de alocacao");
+    }
+    total++;
+    tree->color = RED;
+    tree->naochave = x;
+    tree->parent = parent;
+    tree->left = tree->right = NULL;
+    return tree;
+}
+
+Tree *mySibling(Tree *tree){
+    if (tree->parent->left == tree) return tree->parent->right;
+    return tree->parent->left;
+}
+
+Tree* myAvo(Tree *tree){
+    if(tree != NULL && tree->parent != NULL) return tree->parent->parent;
+    return NULL;
+}
+
+Tree* myUncle(Tree *avo, Tree *tree){
+    if (avo->left == tree->parent) return avo->right;
+    return avo->left;
+}
+
+Tree* updateRoot(Tree *tree){
+    if (tree == NULL) return NULL;
+    while(tree->parent != NULL) tree = tree->parent;
+    return tree;
+}
+
+Tree* search(Tree *tree, unsigned long x){
+
+    while(tree != NULL){
+        if (x == tree->naochave) return tree;
+        if (x < tree->naochave) tree = tree->left;
+        else tree = tree->right;
+    }
+
+    return NULL;
+}
+
+
+void rotate_left(Tree *tree){
+    if (tree->parent != NULL)
+        if (tree == tree->parent->left)
+            tree->parent->left = tree->right;
+        else
+            tree->parent->right = tree->right;
+
+    tree->right->parent = tree->parent;
+    tree->parent = tree->right;
+    tree->right = tree->right->left;
+    tree->parent->left = tree;
+    if (tree->right) tree->right->parent = tree;
+}
+
+void rotate_right(Tree *tree){
+    if (tree->parent != NULL)
+        if (tree == tree->parent->left)
+            tree->parent->left = tree->left;
+        else
+            tree->parent->right = tree->left;
+
+    tree->left->parent = tree->parent;
+    tree->parent = tree->left;
+    tree->left = tree->left->right;
+    tree->parent->right = tree;
+    if (tree->left) tree->left->parent = tree;
+}
+
+void treat_inclusion(Tree *tree){
+    if (tree->parent == NULL){
+        tree->color = BLACK;
+        return;
+    }
+    if (tree->parent->color == BLACK) return;
+
+    Tree *avo = myAvo(tree);
+    Tree *uncle = myUncle(avo, tree);
+    if (uncle != NULL && uncle->color == RED){
+        uncle->color = tree->parent->color = BLACK;
+        avo->color = RED;
+        treat_inclusion(avo);
+        return;
+    }
+
+    if (tree == tree->parent->right && tree->parent == avo->left){
+        rotate_left(tree->parent);
+        tree = tree->left;
+    }else if (tree == tree->parent->left && tree->parent == avo->right){
+        rotate_right(tree->parent);
+        tree = tree->right;
+    }
+
+    tree->parent->color = BLACK;
+    avo->color = RED;
+    if (tree == tree->parent->left) rotate_right(avo);
+    else rotate_left(avo);
+}
+
+Tree* insert(Tree *tree, unsigned long x){
+
+    if (tree == NULL) {
+        tree = newNode(NULL, x);
+        tree->color = BLACK;
+        return tree;
+    }
+
+    Tree *head = tree;
+    while(tree->naochave != x){
+        if (x < tree->naochave){
+            if (tree->left != NULL) tree = tree->left;
+            else {
+                tree->left = newNode(tree, x);
+                break;
+            }
+        }else{
+            if (tree->right != NULL) tree = tree->right;
+            else {
+                tree->right = newNode(tree, x);
+                break;
+            }
+        }
+    }
+
+    if (tree->naochave == x) return head;    //means that the element already inserted and nothing was do
+
+    if (tree->left != NULL && tree->left->naochave == x) treat_inclusion(tree->left);
+    else treat_inclusion(tree->right);
+
+    return updateRoot(head);
+}
+
+
+void print(Tree *tree, int height){
+
+    if (tree == NULL) return;
+
+    print(tree->left, height+1);
+    printf("%lu cor: %d Altura: %d\n", tree->naochave, tree->color, height);
+    print(tree->right, height+1);
+}
+
+int check(Tree *tree, int color){
+
+    if (tree == NULL) return 0;
+
+    total--;
+    if (color && tree->color){
+        printf("Consecutive red in %d and %d\n", tree->color, tree->parent->color);
+        return -1;
+    }
+
+    int l, r;
+    l = check(tree->left, tree->color);
+    if (last >= tree->naochave){
+        printf("Err in %d and %d\n", last, tree->naochave);
+    }
+    last = tree->naochave;
+    r = check(tree->right, tree->color);
+
+    if (l != r){
+        if (l != -1 && r != -1)
+            printf("Err in quantity of black childs, starting the parent %d\n", tree->naochave);
+        return -1;
+    }
+
+    if (tree->color == BLACK) l++;
+    return l;
 }
--- redblack.h
+++ redblack.h
@@ -1,20 +1,16 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "lista.c"
 
+#define N (1 << 24)
 #define BLACK 0
 #define RED 1
 
+int last, total;
 
-typedef struct lista{
-  unsigned long chave;
-  int ind;
-  struct lista * prox;
-}rep;
-
-typedef struct redblack{
-  unsigned long naochave
-  rep *rep_chave;
-  struct tAVL * dir;
-  struct tAVL * esq;
-  int fb;
-}rb;
+typedef struct Tree{
+    unsigned short int color;
+    unsigned long naochave;
+    rep* rep_chave;
+    struct Tree *left, *right, *parent;
+}Tree;
